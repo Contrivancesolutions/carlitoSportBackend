80eb9b4e85c8f2db40e7f53638c8ff330ebdac68
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2020-02-15 19:10:33 +0100

    Lint all the files
    This assures we have nice formatted files before doing any major changes

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,12 @@
 from .models import User
 from django.contrib import admin
 
+
 class UserAdmin(admin.ModelAdmin):
-	search_fields = ['email']
-	class Meta:
-		Model = User 
+    search_fields = ['email']
+
+    class Meta:
+        Model = User
+
 
 admin.site.register(User, UserAdmin)
--- main/views.py
+++ main/views.py
@@ -10,121 +10,143 @@ from django.utils import timezone
 from .models import Abonnement
 User = get_user_model()
 
+
 def homepage(request):
-	return render(request=request, template_name="main/index.html")
+    return render(request=request, template_name="main/index.html")
+
 
 def register(request):
-	if request.method == 'POST':
-		form = InscriptionForm(request.POST)
-		if form.is_valid():
-			data = form.cleaned_data
-			if User.object.filter(email=data['email'])==None:
-				if data['email'] == data['emailConf']:
-					if data['password1'] == data['password2']:
-						user = form.save()
-						login(request, user)
-						abonnement = Abonnement(user=user)
-						abonnement.save()
-						return redirect("main:register2")
-					else:
-						messages.error(request, 'Les mots de passe ne correspondent pas')
-				else:
-					messages.error(request, 'Les emails ne correspondent pas')
-			else:
-				messages.error(request, 'Cet email est déjà utilisé')
-		else:
-			messages.error(request, 'Les informations ne correspondent pas au format demandé')
+    if request.method == 'POST':
+        form = InscriptionForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            if User.object.filter(email=data['email']) == None:
+                if data['email'] == data['emailConf']:
+                    if data['password1'] == data['password2']:
+                        user = form.save()
+                        login(request, user)
+                        abonnement = Abonnement(user=user)
+                        abonnement.save()
+                        return redirect("main:register2")
+                    else:
+                        messages.error(
+                            request, 'Les mots de passe ne correspondent pas')
+                else:
+                    messages.error(request, 'Les emails ne correspondent pas')
+            else:
+                messages.error(request, 'Cet email est déjà utilisé')
+        else:
+            messages.error(
+                request, 'Les informations ne correspondent pas au format demandé')
 
-				
-	form = InscriptionForm().as_p()
-	return render(request, "main/inscription.html", context={"form": form}) if not(request.user.is_authenticated) else redirect("main:homepage")
+    form = InscriptionForm().as_p()
+    return render(request, "main/inscription.html", context={"form": form}) if not(request.user.is_authenticated) else redirect("main:homepage")
+
 
 def logout_request(request):
-	logout(request)
-	return redirect("main:homepage")
+    logout(request)
+    return redirect("main:homepage")
+
 
 def login_request(request):
-	if request.method == 'POST':
-		form = AuthenticationForm(request, data=request.POST )
-		if form.is_valid():
-			email = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=email, password=password)
-			if user is not None:
-				login(request, user)
-				return redirect("main:homepage")				
-			else:
-				messages.error(request, "Les donnés entrées ne correspondent à aucun client")
-		else:
-			messages.error(request, "Mot de passe ou email incorrect")
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("main:homepage")
+            else:
+                messages.error(
+                    request, "Les donnés entrées ne correspondent à aucun client")
+        else:
+            messages.error(request, "Mot de passe ou email incorrect")
 
-	form = AuthenticationForm().as_p()
-	return render(request, "main/login.html", context={"form": form}) if not(request.user.is_authenticated) else redirect("main:homepage")
+    form = AuthenticationForm().as_p()
+    return render(request, "main/login.html", context={"form": form}) if not(request.user.is_authenticated) else redirect("main:homepage")
+
 
 def register2(request):
-	return render(request, "main/inscription2.html")
+    return render(request, "main/inscription2.html")
+
 
 def bonus(request):
-	return render(request, "main/bonus.html")
+    return render(request, "main/bonus.html")
+
 
 def certification(request):
-	return render(request, "main/certification.html")
+    return render(request, "main/certification.html")
+
 
 def contact(request):
-	if request.method == 'POST':
-		form = ContactForm(request, data=request.POST)
-		if form.is_valid():
-			firstName = form.cleaned_data.get('firstName')
-			lastName = form.cleaned_data.get('lastName')
-			email = form.cleaned_data.get('email')
-			subject = form.cleaned_data.get('subject')
-			message = form.cleaned_data.get('message')
-			msg = "De {} {}\n{}".format(lastName, firstName, message)
+    if request.method == 'POST':
+        form = ContactForm(request, data=request.POST)
+        if form.is_valid():
+            firstName = form.cleaned_data.get('firstName')
+            lastName = form.cleaned_data.get('lastName')
+            email = form.cleaned_data.get('email')
+            subject = form.cleaned_data.get('subject')
+            message = form.cleaned_data.get('message')
+            msg = "De {} {}\n{}".format(lastName, firstName, message)
 
-			send_mail(subject, message, EMAIL_HOST_USER, ['azimgivron@gmail.com'])
-		else:
-			messages.error(request, "Les données entrées ne correspondent pas aux formats attendus")
-	form = ContactForm().as_p()
-	return render(request, "main/contact.html", context={"form": form})
+            send_mail(subject, message, EMAIL_HOST_USER,
+                      ['azimgivron@gmail.com'])
+        else:
+            messages.error(
+                request, "Les données entrées ne correspondent pas aux formats attendus")
+    form = ContactForm().as_p()
+    return render(request, "main/contact.html", context={"form": form})
+
 
 def news(request):
-	return render(request, "main/news.html")
+    return render(request, "main/news.html")
+
 
 def faq(request):
-	return render(request, "main/FAQ.html")
+    return render(request, "main/FAQ.html")
+
 
 def pronos(request):
-	return render(request, "main/pronos.html")
+    return render(request, "main/pronos.html")
+
 
 def article1(request):
-	return render(request, "main/article1.html")
+    return render(request, "main/article1.html")
+
 
 def article2(request):
-	return render(request, "main/article2.html")
+    return render(request, "main/article2.html")
+
 
 def article3(request):
-	return render(request, "main/article3.html")
+    return render(request, "main/article3.html")
+
 
 def article4(request):
-	return render(request, "main/article4.html")
+    return render(request, "main/article4.html")
+
 
 def article5(request):
-	return render(request, "main/article5.html")
+    return render(request, "main/article5.html")
+
 
 def article6(request):
-	return render(request, "main/article6.html")
+    return render(request, "main/article6.html")
+
 
 def article7(request):
-	return render(request, "main/article7.html")
+    return render(request, "main/article7.html")
+
 
 def article8(request):
-	return render(request, "main/article8.html")
+    return render(request, "main/article8.html")
+
 
 def article9(request):
-	return render(request, "main/article9.html")
+    return render(request, "main/article9.html")
+
 
 def article10(request):
-	return render(request, "main/article10.html")
-
-
-
+    return render(request, "main/article10.html")
